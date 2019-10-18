211d171ca1ef2d1195220de75d712776fc72f663
Шрек <55573385+eKulakov228@users.noreply.github.com>
2019-10-18 09:38:34 +0300

    Update task2lab3.c

--- Task 2/task2lab3.c
+++ Task 2/task2lab3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    int C=0,S=100,a,b,c,d; // a - 50, b - 20, c - 5, d - 2
+    int C=0,S=100,a,b,c,d; // a - 50, b - 20, c - 5, d - 2, S - 100
     for(a=0;a<=2;++a)
     {
         for(b=0;b<=5;++b)
